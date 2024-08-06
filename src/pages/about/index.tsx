b0541badddedb37e518fe34b477e7523f8db1019
marcoslima12 <marcosantonio-10@live.com>
2024-08-06 10:33:37 -0300

    remove logs

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -53,54 +53,52 @@ const About = ({ dot }: SliderTypes) => {
   ];
 
   return (
-    <div className="bg-light-primary h-screen dark:bg-dark-primary  border-2 border-red-600 ">
+    <div className="bg-light-primary h-screen dark:bg-dark-primary  border-2 ">
       <Header />
-      <div className="w-full h-full flex items-center justify-center gap-32 border-2 border-blue-600">
-          <div className="w-1/3 ">
-            <Slider
-              className="rounded-lg flex flex-col justify-center"
-              {...sliderSettings}
-            >
-              {images.map((image, index) => {
-                return (
-                  <div
-                    key={index}
-                    className="p-1 flex flex-important justify-center"
-                  >
-                    <Image
-                      src={image}
-                      alt="Foto do carrosel"
-                      className="rounded-lg w-auto h-[500px] box-border shadow shadow-dark-primary"
-                    />
-                  </div>
-                );
-              })}
-            </Slider>
-          </div>
+      <div className="w-full h-full flex items-center justify-center gap-32 border-2">
+        <div className="w-1/3 ">
+          <Slider
+            className="rounded-lg flex flex-col justify-center"
+            {...sliderSettings}
+          >
+            {images.map((image, index) => {
+              return (
+                <div
+                  key={index}
+                  className="p-1 flex flex-important justify-center"
+                >
+                  <Image
+                    src={image}
+                    alt="Foto do carrosel"
+                    className="rounded-lg w-auto h-[500px] box-border shadow shadow-dark-primary"
+                  />
+                </div>
+              );
+            })}
+          </Slider>
+        </div>
 
-          <div className="flex flex-col w-80 gap-10">
-            <h1 className="text-7xl font-bold text-left text-light-text dark:text-dark-text">
-              Quem sou eu?
-            </h1>
-            <p className="text-xl text-light-text dark:text-dark-text">
-              Também chamado de{" "}
-              <span className="text-dark-highlight font-bold">Marcante</span>,
-              sou filho de Antônio e Rosineide.
-              <br />
-              Natural de Timbaúba, interior de Pernambuco, sou fã de música e
-              apaixonado por conexões e experiências. Gosto de praticar corrida,
-              andar e bicicleta e fazer academia, além de cantar e desenhar. Na
-              verdade, faço de tudo um pouco, mesmo que não seja o melhor
-              naquilo. Acredito que bom mesmo é experimentar. Por isso, me
-              considero um
-              <span className="text-dark-highlight font-bold">
-                {" "}
-                experimentador da vida
-              </span>
-              .
-            </p>
-          </div>
-       
+        <div className="flex flex-col w-80 gap-10">
+          <h1 className="text-7xl font-bold text-left text-light-text dark:text-dark-text">
+            Quem sou eu?
+          </h1>
+          <p className="text-xl text-light-text dark:text-dark-text">
+            Também chamado de{" "}
+            <span className="text-dark-highlight font-bold">Marcante</span>, sou
+            filho de Antônio e Rosineide.
+            <br />
+            Natural de Timbaúba, interior de Pernambuco, sou fã de música e
+            apaixonado por conexões e experiências. Gosto de praticar corrida,
+            andar e bicicleta e fazer academia, além de cantar e desenhar. Na
+            verdade, faço de tudo um pouco, mesmo que não seja o melhor naquilo.
+            Acredito que bom mesmo é experimentar. Por isso, me considero um
+            <span className="text-dark-highlight font-bold">
+              {" "}
+              experimentador da vida
+            </span>
+            .
+          </p>
+        </div>
       </div>
     </div>
   );
